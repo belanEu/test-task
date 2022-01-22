9eb7d393ccf2d1a51626a25d840b77b35d18e22d
belanEu <evgeniy.belan.97@mail.ru>
2022-01-22 18:15:50 +0300

    underline chosen tab

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -1,9 +1,24 @@
 import './Nav.sass';
+import cn from 'classnames';
 
-export const Nav = ({ tabs }) => {
+export const Nav = ({ tabs, chosenTab }) => {
     return (
         <nav>
-            { tabs.map(tab => (<div key={tab} className="item">{ tab }</div>)) }
+            {
+            Object.keys(tabs).map(tab => {
+                const isChosen = tab === chosenTab;
+                return (
+                <div key={tab} className={cn('item', {chosen: isChosen})}>
+                    <a className={cn({chosen: isChosen})}
+                        href={isChosen ? '' : `/?tab=${tab}`}
+                    >
+                        { tabs[tab] }
+                    </a>
+                </div>
+                )
+
+            })
+            }
         </nav>
     );
 };

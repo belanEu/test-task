7d51aaf7c8d178be2af4f9c486f0948275279d1f
belanEu <evgeniy.belan.97@mail.ru>
2022-01-22 14:48:13 +0300

    created EmptyList

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,10 +1,12 @@
 import { Book } from "./Book";
+import { EmptyList } from "./EmptyList";
 import './BookList.sass';
 
 export const BookList = ({books}) => {
     return (
         <section className="book-list">
             { books.map(book => <Book key={book.id} data={book} />) }
+            { books.length === 0 ? <EmptyList /> : '' }
         </section>
     );
 };
---
+++ frontend/src/components/BookList/EmptyList.js
@@ -0,0 +1,9 @@
+import './BookList.sass';
+
+export const EmptyList = () => {
+    return (
+        <div className='empty-list'>
+            List is empty
+        </div>
+    );
+};

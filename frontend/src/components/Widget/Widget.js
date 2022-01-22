d94b1fb8237aa49e2f87226dc66bf4e504a7f04c
belanEu <evgeniy.belan.97@mail.ru>
2022-01-22 18:18:43 +0300

    used functions from stubs and utils

--- frontend/src/components/Widget/Widget.js
+++ frontend/src/components/Widget/Widget.js
@@ -1,14 +1,14 @@
 import { Nav } from '../Nav/Nav';
 import { BookList } from '../BookList/BookList';
-
 import './Widget.sass';
-import { getBooks } from '../../stubs';
+import { getBooks, getTabs } from '../../stubs';
+import { getTabFromUrl, getTagsFromUrl } from '../../utils';
 
 export const Widget = () => {
     return (
         <div className="widget">
-            <Nav tabs={['To read', 'In progress', 'Done']} />
-            <BookList books={getBooks()} />
+            <Nav tabs={getTabs()} chosenTab={getTabFromUrl()} />
+            <BookList books={getBooks()} tags={getTagsFromUrl()} />
         </div>
     );
 };

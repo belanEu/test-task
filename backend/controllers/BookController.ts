176dc3fa5fbf55210fe036d0abb5a29518618a3e
belanEu <evgeniy.belan.97@mail.ru>
2022-01-23 23:21:52 +0300

    fixed BookController actions

--- backend/controllers/BookController.ts
+++ backend/controllers/BookController.ts
@@ -2,9 +2,21 @@ import { FastifyRequest } from 'fastify';
 import { loggerNamespace } from 'cleverJS/build/core/logger/logger';
 import { HttpServer } from 'cleverJS/build/core/http/HttpServer';
 import { Paginator } from 'cleverJS/core/utils/Paginator';
+import { BookService } from './../modules/BookService';
 
 interface IDependencies {
-    http: HttpServer
+    http: HttpServer,
+    bookService: BookService
+}
+
+interface ChangeBookStatusRequest {
+    bookId: number,
+    status: string
+}
+
+interface PaginatorParams {
+    page: number,
+    itemsPerPage: number
 }
 
 export class BookController {
@@ -16,14 +28,32 @@ export class BookController {
         this.initRoutes();
     }
 
-    protected async test(request: FastifyRequest) {
-        return 'test';
+    protected actionBookList = async(request: FastifyRequest) => {
+        const { page, itemsPerPage } = request.query as PaginatorParams;
+
+        const paginator = new Paginator();
+        paginator.setCurrentPage(page);
+        paginator.setItemsPerPage(itemsPerPage);
+
+        const result = await this.deps.bookService.fetchBookList(paginator);
+
+        return {
+            success: true,
+            data: {
+                result,
+            },
+        };
+    }
+
+    protected actionChangeBookStatus = async(request: FastifyRequest) => {
+        const { bookId, status } = request.body as ChangeBookStatusRequest;
+        // todo: change status
     }
 
     protected initRoutes() {
         const app = this.deps.http.getServer();
         
-        app.get('/api/test', this.test);
+        app.get('/api/books', this.actionBookList);
+        app.post('/api/book_status', this.actionChangeBookStatus);
     }
 }
-

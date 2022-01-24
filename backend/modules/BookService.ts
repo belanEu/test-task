a9693c3dc294527c28bcdc3c2133b238ba61887a
belanEu <evgeniy.belan.97@mail.ru>
2022-01-25 01:41:49 +0300

    created method fetchFilteredBookList

--- backend/modules/BookService.ts
+++ backend/modules/BookService.ts
@@ -4,11 +4,32 @@ import { AbstractService } from "cleverJS/build/core/AbstractService";
 import { Paginator } from "cleverJS/build/core/utils/Paginator";
 import { Condition, IConditionItemList, TConditionOperator } from "cleverJS/build/core/db/Condition";
 
+type FilterBy = {
+    status: string,
+    tags: string[]
+};
+
 export class BookService extends AbstractService<Book, BookResource> {
     public fetchBookList(bookStatus: string, paginator: Paginator) {
         const condition = {
             conditions: [{ operator: TConditionOperator.EQUALS, field: 'status', value: bookStatus }]
         } as IConditionItemList;
         return this.resource.findBooksWithPagination(new Condition(condition), paginator);
+    }
+
+    public async fetchFilteredBookList(filterBy: FilterBy, paginator: Paginator) {
+        const condition = {
+            conditions: [{ operator: TConditionOperator.EQUALS, field: 'status', value: filterBy.status }]
+        } as IConditionItemList;
+        const tagsMap = {};
+        filterBy.tags.map(tag => tagsMap[tag] = true);
+
+        const allBooks = await this.resource.findAllBooks(new Condition(condition));
+
+        const start = paginator.getOffset(), end = start + paginator.getLimit();
+
+        return allBooks
+            .filter(book => book.tags.some(tag => tagsMap[tag] !== undefined))
+            .slice(start, end);
     }
 }

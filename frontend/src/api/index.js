c8297f7a949718aa9e0060fbfca50a48dc162483
belanEu <evgeniy.belan.97@mail.ru>
2022-01-27 23:54:14 +0300

    created fetchWrapper method

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,25 +2,28 @@ const BASE_URL = 'http://localhost:8000/api';
 
 const alertMessage = 'Что-то пошло не так! Просим связаться с тех. поддержкой';
 
+const fetchWrapper = async cb => {
+    try {
+        const res = await cb();
+        if (!res.ok) {
+            throw new Error();
+        }
+        const data = await res.json();
+        return data ? data.data.result : [];
+    } catch (err) {
+        alert(alertMessage);
+    }
+};
+
 export const api = {
     getStatusCounters: async () => {
-        try {
-            const res = await fetch(`${BASE_URL}/status_counters`);
-            const data = await res.json();
-            return data.data.result;
-        } catch (err) {
-            alert(alertMessage);
-        }
+        return await fetchWrapper(() => fetch(`${BASE_URL}/status_counters`));
     },
-    getTabBooks: async (tab, page, itemsPerPage = 4) => {
-        try {
-            const res = await fetch(`${BASE_URL}/books?status=${tab}&page=${page}&itemsPerPage=${itemsPerPage}`);
-            const data = await res.json();
-            return data.data.result;
-        } catch (err) {
-            alert(alertMessage);
-        }
+    getTabBooks: async (tab, page = 1, itemsPerPage = 4) => {
+        return await fetchWrapper(() => fetch(`${BASE_URL}/books?status=${tab}&page=${page}&itemsPerPage=${itemsPerPage}`));
     },
-    getFilteredBooks: async () => {},
-    changeBookStatus: async () => {},
+    getFilteredBooks: async (tab, tags, page = 1, itemsPerPage = 4) => {
+        return await fetchWrapper(() => fetch(`${BASE_URL}/filtered_books?status=${tab}&tags=${tags}&page=${page}&itemsPerPage=${itemsPerPage}`));
+    },
+    changeBookStatus: async (bookId, status) => {},
 };

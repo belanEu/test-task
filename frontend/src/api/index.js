09f253b1caa7d3f7baa7718d3aec7a992f0bcc82
belanEu <evgeniy.belan.97@mail.ru>
2022-01-28 00:14:15 +0300

    initialized default values

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -15,14 +15,16 @@ const fetchWrapper = async cb => {
     }
 };
 
+const DEFAULT_PAGE = 1, DEFAULT_ITEMS_PER_PAGE = 5;
+
 export const api = {
     getStatusCounters: async () => {
         return await fetchWrapper(() => fetch(`${BASE_URL}/status_counters`));
     },
-    getTabBooks: async (tab, page = 1, itemsPerPage = 4) => {
+    getTabBooks: async (tab, page = DEFAULT_PAGE, itemsPerPage = DEFAULT_ITEMS_PER_PAGE) => {
         return await fetchWrapper(() => fetch(`${BASE_URL}/books?status=${tab}&page=${page}&itemsPerPage=${itemsPerPage}`));
     },
-    getFilteredBooks: async (tab, tags, page = 1, itemsPerPage = 4) => {
+    getFilteredBooks: async (tab, tags, page = DEFAULT_PAGE, itemsPerPage = DEFAULT_ITEMS_PER_PAGE) => {
         return await fetchWrapper(() => fetch(`${BASE_URL}/filtered_books?status=${tab}&tags=${tags}&page=${page}&itemsPerPage=${itemsPerPage}`));
     },
     changeBookStatus: async (bookId, status) => {},

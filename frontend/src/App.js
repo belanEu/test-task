004073ea3c1d462b2e5ab5e61d3bc7c317904716
belanEu <evgeniy.belan.97@mail.ru>
2022-01-29 14:18:34 +0300

    handling changes of book status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,35 @@
 import { useState, useEffect } from 'react';
 import './App.sass';
-import { Widget } from './components/Widget/Widget';
+import { Nav } from './components/Nav/Nav';
+import { BookList } from './components/BookList/BookList';
 import { api } from './api';
 import { getTabFromUrl, getTagsFromUrl } from './utils';
+
+const defaultBookStatus = {id: -1, status: 'none'};
 
 function App() {
   const [tabs, setTabs] = useState([]);
   const [books, setBooks] = useState([]);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [book, setNextStatus] = useState(defaultBookStatus);
+
   const currentTab = getTabFromUrl(), tags = getTagsFromUrl();
+
+  const updateWidgetByChangingBookStatus = (bookId, status) => {
+    const index = books.findIndex(item => item.id === bookId);
+    books.splice(index, 1);
+
+    const nextTab = tabs.find(item => item.id === status),
+    currTab = tabs.find(item => item.id === currentTab);
+    nextTab.count++;
+    currTab.count--;
+    
+    setNextStatus({id: bookId, status});
+    setTabs(tabs);
+    setBooks(books);
+  };
 
   useEffect(() => {
     const fetchTabs = async () => {
@@ -28,7 +48,6 @@ function App() {
       setBooks(data);
     };
     const fetchFilteredBooks = async () => {
-      console.log('fetch filtered books');
       setIsLoading(true);
       const data = await api.getFilteredBooks(currentTab, tags, page);
       setIsLoading(false);
@@ -46,12 +65,28 @@ function App() {
   }, [page]);
 
   useEffect(() => {
-    
-  }, [page]);
+    const changeBookStatus = async () => {
+      if (book.id !== defaultBookStatus.id) {
+        const res = await api.changeBookStatus(book.id, book.status);
+        if (!res) {
+          alert("Не удалось изменить статус книги! Просим связаться с тех. поддержкой");
+        }
+      }
+    };
+
+    changeBookStatus();
+  }, [book.id]);
 
   return (
     <div className="app">
-      {isLoading ? 'Loading ...' : <Widget tabs={tabs} books={books} tags={tags} />}
+      {isLoading ? 'Loading ...' :
+      <div className="widget">
+        <Nav tabs={tabs} chosenTab={currentTab} />
+        <BookList books={books} tags={tags}
+        handleChangeBookStatus={(id, status) => updateWidgetByChangingBookStatus(id, status)}
+        />
+      </div>
+      }
     </div>
   );
 }
